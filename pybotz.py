c579a8ca9510e3ee884603bcf374c037c6c6ef47
Joshua R. Heling <jrh@netfluvia.org>
2015-01-10 09:17:11 -0600

    cleaned up markdown

--- pybotz.py
+++ pybotz.py
@@ -18,7 +18,7 @@
 of APC's Netbotz 500 monitoring hardware[1]. 
 
 Classes
-=============================================================================
+--------
 CheckerPool - simple pool of SensorModuleCheckers
 SensorChecker - logic and state related to a single sensor
 SensorModuleChecker - performance-oriented grouping of sensors to common 
@@ -26,12 +26,12 @@ SensorModuleChecker - performance-oriented grouping of sensors to common
 SensorReading - complex data type for data read from a sensor
 
 Functions
-=============================================================================
+--------
 get_sensor_modules() - identify all modules on a given netbotz host
 scrape_sensor_module() - get all readings from an identified sensor module
 
 Terminology and Conceptual Organization of Netbotz Components
-=============================================================================
+----------------------------------
 Some familiarity with netbotz hardware is assumed here -- see [1] for more
 background, if necessary.  
 
@@ -42,7 +42,7 @@ will provide one or more types of data (e.g. Temperature, Dew Point, etc. for
 a "SensorPod 120"); each of these is referred to as a "sensor".
 
 Required MySQL Schema
-=============================================================================
+----------------------------------
 This module expects to see a description of the netbotz environment it is 
 scraping in a MySQL database with the following schema:
 
@@ -92,14 +92,14 @@ ENGINE = InnoDB;
  - - - - - end mysql schema - - - - -
 
 Tested Hardware
-=============================================================================
+----------------------------------
 Testing was done on an installation with two Netbotz 500 appliances and a 
 combination of Camera Pod 120 and Sensor Pod 120 modules.  Other hardware 
 details will probably work, perhaps with trivial changes to the code, but no
 serious thought has been given to this.
 
 Compatibility
-=============================================================================
+----------------------------------
 Tested on MacOS and Linux.  Signal handling won't work on Windows without
 some changes.
 
